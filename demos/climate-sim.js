61da543a26d6d545bfeb5edfce777f437ef7bc2c
TeomanDavid <davidteoman@gmail.com>
2016-07-11 02:11:11 -0700

    restructured CSS and added info section

--- demos/climate-sim.js
+++ demos/climate-sim.js
@@ -24,7 +24,7 @@ var map;
 const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
 const tempAnomaly = {1850:-0.431,1851:-0.020,1852:-0.320,1853:-0.388,1854:0.065,1855:-0.188,1856:-0.352,1857:-0.289,1858:-0.375,1859:-0.363,1860:-0.326,1861:-0.266,1862:-0.591,1863:-0.307,1864:-0.639,1865:-0.306,1866:-0.358,1867:-0.371,1868:-0.291,1869:-0.260,1870:-0.266,1871:-0.413,1872:-0.164,1873:-0.204,1874:-0.343,1875:-0.584,1876:-0.245,1877:-0.040,1878:0.109,1879:-0.409,1880:-0.169,1881:-0.303,1882:-0.138,1883:-0.412,1884:-0.541,1885:-0.500,1886:-0.423,1887:-0.522,1888:-0.488,1889:-0.228,1890:-0.467,1891:-0.599,1892:-0.603,1893:-0.689,1894:-0.551,1895:-0.551,1896:-0.380,1897:-0.301,1898:-0.405,1899:-0.330,1900:-0.176,1901:-0.187,1902:-0.352,1903:-0.435,1904:-0.570,1905:-0.434,1906:-0.224,1907:-0.619,1908:-0.493,1909:-0.471,1910:-0.346,1911:-0.469,1912:-0.392,1913:-0.326,1914:-0.063,1915:-0.082,1916:-0.370,1917:-0.694,1918:-0.489,1919:-0.277,1920:-0.306,1921:-0.168,1922:-0.261,1923:-0.295,1924:-0.370,1925:-0.280,1926:-0.046,1927:-0.231,1928:-0.164,1929:-0.444,1930:-0.137,1931:-0.125,1932:-0.056,1933:-0.296,1934:-0.071,1935:-0.167,1936:-0.111,1937:-0.071,1938:0.109,1939:-0.059,1940:-0.030,1941:-0.013,1942:-0.028,1943:-0.064,1944:0.073,1945:-0.100,1946:-0.078,1947:-0.009,1948:-0.059,1949:-0.150,1950:-0.318,1951:-0.125,1952:-0.037,1953:0.058,1954:-0.186,1955:-0.204,1956:-0.433,1957:-0.052,1958:0.081,1959:-0.011,1960:-0.100,1961:0.040,1962:-0.000,1963:0.007,1964:-0.285,1965:-0.185,1966:-0.116,1967:-0.120,1968:-0.210,1969:-0.059,1970:-0.022,1971:-0.205,1972:-0.176,1973:0.156,1974:-0.306,1975:-0.114,1976:-0.368,1977:0.072,1978:-0.046,1979:0.062,1980:0.141,1981:0.248,1982:0.021,1983:0.320,1984:-0.058,1985:-0.010,1986:0.117,1987:0.290,1988:0.342,1989:0.195,1990:0.428,1991:0.339,1992:0.103,1993:0.183,1994:0.326,1995:0.477,1996:0.215,1997:0.464,1998:0.821,1999:0.493,2000:0.363,2001:0.559,2002:0.666,2003:0.645,2004:0.622,2005:0.760,2006:0.674,2007:0.680,2008:0.527,2009:0.672}; 
 
-const dataStartYear = 1900;                     // year the data set starts, inclusive
+const dataStartYear = 1850;                     // year the data set starts, inclusive
 const dataEndYear = 2010;                       // year the data set ends, not inclusive
 const defaultStyle = 'solid';                   // default display mode for the map ('solid' or 'heatmap')
 const defaultStartYear = 2000;                  // year to start the display
@@ -78,6 +78,8 @@ function initSelect(id, source, selected, handler){ source.forEach(function(item
 function loadTemperatureScales() {var elements = ['#raw-temp-start', '#raw-temp-end', '#anomaly-start', '#anomaly-end']; var temps = [tempRange[0], tempRange[1], anomalyRange[0], anomalyRange[1]]; elements.forEach(function(ele, index){ $(ele).html(temps[index] + "&deg;C"); }); }
 // toggles animation when button is pushed
 function toggleAnimation(){ if(playing) { $('#playpause').text("Play"); window.clearInterval(intervalID);}else { $('#playpause').text("Stop"); intervalID = window.setInterval(function(){ if(currentYear < dataEndYear - 1){ currentYear++; }else if(loop){ currentYear = dataStartYear;} updateMap(); }, animationSpeed); } playing = !playing;}
+// shows an alert w/author information
+function showInfo(e){e.preventDefault(); alert("HADCrut3 Climate Simulation by Teoman (Ted) Yavuzkurt.\nhttp://www.github.com/TeomanDavid\nhttp://www.teomandavid.com\n\nSource available on GitHub under MIT license (my portions).\nRaw Data From: http://www.metoffice.gov.uk/research/climate/climate-monitoring/land-and-atmosphere/surface-station-records\n\nPOWERED BY MAPBOXGL: http://www.mapbox.com");}
 
 // ###### STYLES #####
 
@@ -219,7 +221,7 @@ $(document).ready(function(){
   // instantiate the map with some basic parameters that work well for this data set
   map = new mapboxgl.Map({
       container: 'map',
-      style: 'mapbox://styles/teomandavid/ciqgbmop7001bcfnnag7yupxd',
+      style: 'mapbox://styles/teomandavid/ciqhsdrro002qcfnn41ofo4f2',
       zoom: 2,
       minZoom: 2,
       maxZoom: 7,
@@ -371,14 +373,17 @@ $(document).ready(function(){
     $('#playpause').on('click', toggleAnimation);
 
     // when we change the checkbox, toggle showAnomaly, and redraw
-    $('#toggleAnomaly').on('change', function(){
+    $('#toggle-anomaly').on('change', function(){
       showAnomaly = !showAnomaly;
       updateAnomaly(); 
     });
 
     // when we change the checkbox, toggle loop
     // has no immediate effect unless animation is paused on last year
-    $('#toggleLoop').on('change', function() { loop = !loop; });
+    $('#toggle-loop').on('change', function() { loop = !loop; });
+
+    // show author information
+    $('#info-button').on('click', showInfo);
 
     // and that's it! Not so bad :)
   });
